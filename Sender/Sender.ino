9681012126519b4aa75e1660d1273486e413c49e
Evan Serre <evaserr@nhs10691-10-evaserr.nueva.local>
2018-02-12 16:48:53 -0800

    Fixed dual Teensy CAN Bus communications errors and added status LED

--- Sender/Sender.ino
+++ Sender/Sender.ino
@@ -1,14 +1,16 @@
 #include <FlexCAN.h>
 #include <TeensyCANBase.h>
-int pin = 25;
+
+int toggle = 0;
+
 void setup(void) {
   CAN_begin();
-  pinMode(pin, OUTPUT);
   Serial.begin(9600);
+  pinMode(13, OUTPUT);
   delay(1000);
   Serial.println("Teensy 3.X CAN Sender");
-  digitalWrite(pin, HIGH);
-  
+  digitalWrite(13, HIGH);
+  toggle = 1;
 }
 
 void writeLongs(uint32_t id, long value1, long value2){
@@ -24,10 +26,18 @@ void writeLongs(uint32_t id, long value1, long value2){
   CAN_write(id, msg);
 
   delete msg;
+  Serial.println("sent");
+  if (toggle == 1){
+    digitalWrite(13, LOW);
+    toggle = 0;
+  } else {
+    digitalWrite(13, HIGH);
+    toggle = 1;
+  }
 }
 
 void loop(void) {
   CAN_update();
-  writeLongs(0x672, 101, 202);
-  delay(10);
+  writeLongs(0x612, 101, 202);
+  delay(50);
 }
